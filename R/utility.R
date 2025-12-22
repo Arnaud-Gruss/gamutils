bd7826c854d2ba7e5b4a1e4a2f137e2ee755fcb8
Arnaud Grüss <gruss.arnaud@gmail.com>
2025-12-22 21:54:19 +1300

    Adding the mode_factor and get_max functions

--- R/utility.R
+++ R/utility.R
@@ -7,9 +7,28 @@
 #' @return the mode of a categorical variable
 #'
 #' @export
-modefunc <- function( x ) {
+mode_factor <- function( x ) {
 
   a <- table( x )
   names( a[a == max( a )] )
+
+}
+
+#' @title Maximum element from a vector
+#'
+#' @description This function finds the maximum element from a vector
+#'
+#' @param x vector interest
+#'
+#' @return the maximum element from the vector of interest
+#'
+#' @export
+get_max <- function( x ) {
+
+  if ( all( is.na( x ) ) ) {
+    ( x[1] )
+  } else {
+    x[which( x == max( x, na.rm = T ) )][1]
+  }
 
 }
